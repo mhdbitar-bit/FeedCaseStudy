51046a54bd3dffbb63f623109834edf1197ee6bb
Mohamd Bitar <eng.mhd.bitar@gmail.com>
2022-09-05 08:35:19 +0300

    `CoreDataFeedStore.insert()` delivers no error on empty cache

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -10,7 +10,7 @@ import CoreData
 public final class CoreDataFeedStore: FeedStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
-    
+
     public init(storeURL: URL, bundle: Bundle = .main) throws {
         container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
         context = container.newBackgroundContext()
@@ -46,10 +46,11 @@ public final class CoreDataFeedStore: FeedStore {
             }
         }
     }
-    
+
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
     }
+
 }
 
 private extension NSPersistentContainer {
@@ -57,7 +58,7 @@ private extension NSPersistentContainer {
         case modelNotFound
         case failedToLoadPersistentStores(Swift.Error)
     }
-    
+
     static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
         guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
             throw LoadingError.modelNotFound
@@ -66,7 +67,7 @@ private extension NSPersistentContainer {
         let description = NSPersistentStoreDescription(url: url)
         let container = NSPersistentContainer(name: name, managedObjectModel: model)
         container.persistentStoreDescriptions = [description]
-        
+
         var loadError: Swift.Error?
         container.loadPersistentStores { loadError = $1 }
         try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
@@ -112,7 +113,7 @@ private class ManagedFeedImage: NSManagedObject {
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
             managed.imageDescription = local.description
-            managed.location  = local.location
+            managed.location = local.location
             managed.url = local.url
             return managed
         })
--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -10,40 +10,16 @@ import EssentialFeed
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
-    func test_retrieve_deliversFailureOnRetrievalError() {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
         
         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
-    func test_retrieve_hasNoSideEffectsOnFailure() {
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
         
         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
-    }
-    
-    func test_insert_deliversErrorOnInsertionError() {
-        
-    }
-    
-    func test_insert_hasNoSideEffectsOnInsertionError() {
-        
-    }
-    
-    func test_delete_deliversErrorOnDeletionError() {
-        
-    }
-    
-    func test_delete_hasNoSideEffectsOnDeletionError() {
-        
-    }
-    
-    func test_retrieve_deliversEmptyOnEmptyCache() {
-        
-    }
-    
-    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
@@ -59,37 +35,39 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {
-        
+
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
-        
+
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
-        
-    }
-    
-    func test_delete_deliversNoErrorOnNonEmptyCache() {
-        
+
     }
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
-        
+
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+
     }
     
     func test_delete_emptiesPreviouslyInsertedCache() {
-        
+
     }
     
     func test_storeSideEffects_runSerially() {
-        
+
     }
-    
+
     // - MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
@@ -99,5 +77,5 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
-    
+
 }
